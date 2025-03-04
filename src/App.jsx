18d3b09ec4380712445f3190921bffeb62d167d3
Matheus Meigre <matheusmeigre@hotmail.com>
2025-03-03 22:27:01 -0300

    att useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -16,14 +16,14 @@ function App() {
   localStorage.setItem("tasks", JSON.stringify(tasks))
  }, [tasks])
 
- useEffect(() => {
-  const fetchTasks = async () => {
-    const response = await fetch('https://jsonplaceholder.typicode.com/todos?_limit=10', {
-      method: 'GET'
-    })
-    const data = await response.json()
-  }
- }, [])
+//  useEffect(() => {
+//   const fetchTasks = async () => {
+//     const response = await fetch('https://jsonplaceholder.typicode.com/todos?_limit=10', {
+//       method: 'GET'
+//     })
+//     const data = await response.json()
+//   }
+//  }, [])
 
  function onTaskClick(taskId) {
   const newTasks = tasks.map((task) => {
